b75112397e03afa6c368a950ecaa3206a1726ae2
Roshangeek <86564512+Roshangeek@users.noreply.github.com>
2026-09-20 03:10:45 +0530

    Update prisma.js

--- prisma/src/lib/prisma.js
+++ prisma/src/lib/prisma.js
@@ -1,1 +1,8 @@
+import { PrismaClient } from '@prisma/client'
+
+const prismaGlobal = globalThis.prismaGlobal || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prismaGlobal
+
+export const prisma = prismaGlobal
 
